de999c9829e1228370a63eb16a993e4801af882d
chengpeng <chenpenjob@163.com>
2019-11-25 21:59:31 +0800

    前后端分离的通讯录项目（Vue+Springboot）

---
+++ src/pages/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    通讯录
+    <router-link tag="div" to="/add" class="iconfont add-icon">&#xe81b;</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+  @import "~@/assets/styles/varibles.styl"
+
+  .header
+    position: relative
+    overflow: hidden
+    background: $bgColor
+    font-size: .35rem
+    font-weight: 700
+    text-align: center
+    height: .86rem
+    line-height: .86rem
+    .add-icon
+      position: fixed
+      right: .45rem
+      top: 0rem
+</style>
